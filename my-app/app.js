42c317c6a33a917e8c90bdb5492e68558b9e273b
abhinand-psq <103823872+abhinand-psq@users.noreply.github.com>
2024-12-24 15:21:18 +0530

    third commit

--- my-app/app.js
+++ my-app/app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var sampleRouter = require('./routes/sample')
 var app = express();
 dbs.run()
 
@@ -22,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/sample', sampleRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
